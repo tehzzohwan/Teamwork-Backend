c3f6adfdb1e33bd334d08a05e117d9fa05373897
Bobby Enomate <enomatebobby@.com>
2023-08-05 02:25:52 +0100

    connected db with render

--- backend/api/configs/db.configs.js
+++ backend/api/configs/db.configs.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const db = new Pool({
-	user: process.env.DB_USERNAME,
-	host: 'localhost',
-	database: process.env.DB_NAME,
-	password: process.env.DB_PASSWORD,
-	port: 5432,
+	connectionString: process.env.DBConfigLink,
+	ssl: {
+		rejectUnauthorized: false
+	}
 });
 
 module.exports = db;
