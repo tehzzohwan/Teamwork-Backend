646eb4f83e6bad626dc4a75b175abf78e8898268
Bobby Enomate <enomatebobby@.com>
2023-08-08 03:42:51 +0100

    origin change

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000; 
 
 const corsOptions = {
-	origin: 'https://tehzz-teamwork.netlify.app' 
+	origin: 'https://master--tehzz-teamwork.netlify.app' 
 };
   
 app.use(cors(corsOptions));
